c95a7921f9d59deec16fd1fce8e6287ce938d8ef
Andrew Kelley <iodine73@gmail.com>
2017-10-19 15:20:01 -0400

    Update bamazonManager.js

--- bamazonManager.js
+++ bamazonManager.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
-	user: 'root',
+	user: null,
 	password: 'password123',
 	database: 'bamazon'
 });
